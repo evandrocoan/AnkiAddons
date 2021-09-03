c305ce10354f9a52f309576ca8beb879bacfc131
evandrocoan <evandrocoan@hotmail.com>
2021-09-03 07:14:21 -0300

    Installed the Answer Confirmation for 2.1 addon

---
+++ 1354051622/__init__.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+# Author:  Albert Lyubarsky
+# Email: albert.lyubarsky@gmail.com
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+#
+#   Answer Confirmation plugin for Anki 2.0
+__version__ = "1.0.0"
+
+
+from aqt.reviewer import Reviewer
+from aqt.utils import tooltip
+from anki.hooks import wrap
+
+
+def answerCard_before(self, ease) :
+	l = self._answerButtonList()
+	a = [item for item in l if item[0] == ease]
+	if len(a) > 0 :
+		tooltip(a[0][1])
+		
+
+Reviewer._answerCard  = wrap(Reviewer._answerCard, answerCard_before, "before")
