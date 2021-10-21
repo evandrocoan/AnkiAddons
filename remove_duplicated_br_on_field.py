464601f846aa2a9a69af523e1ac5714242e9265d
evandrocoan <evandrocoan@hotmail.com>
2021-10-20 21:56:13 -0300

    Created remove_duplicated_br_on_field.py

---
+++ remove_duplicated_br_on_field.py
@@ -0,0 +1,14 @@
+
+import re
+field_fixer_regex = re.compile(r'<\s*br\s*\/?\s*>.*')
+
+for nid, in mw.col.db.execute(f"select id from notes"):
+    note = mw.col.getNote(nid)
+    field_to_fix = "Kanji"
+
+    if note.note_type()["name"] == "Japanese Kanji":
+        original_field = note[field_to_fix]
+        fixed_field = field_fixer_regex.sub("", original_field)
+        print(f"Before {original_field} -> {fixed_field}")
+        note[field_to_fix] = fixed_field
+        note.flush()
