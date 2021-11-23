cf9796b48c72314a7cf9eac0494560fee15874fb
evandrocoan <evandrocoan@hotmail.com>
2021-11-23 19:40:17 -0300

    Created change_test_note.py

---
+++ change_test_note.py
@@ -0,0 +1,12 @@
+
+target_field = 'Front'
+
+for nid, in mw.col.db.execute(f"select id from notes where id = 1637706916492"):
+    note = mw.col.getNote(nid)
+    print(f"Changing deck {note.id} from '{note[target_field]}'...")
+
+    note[target_field] = "Changed"
+    note.flush()
+
+    print(f"After changing deck {note.id} with '{note[target_field]}'...")
+    break
