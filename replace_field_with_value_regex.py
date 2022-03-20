52bb5511d76fad8b7913229d83a6b214fb57de23
evandrocoan <evandrocoan@hotmail.com>
2022-03-20 15:43:32 -0300

    Set replace_field_with_value_regex.py to remove trailing slash /

--- replace_field_with_value_regex.py
+++ replace_field_with_value_regex.py
@@ -7,16 +7,16 @@ for name, did in mw.col.db.execute(f"select name, id from decks"):
 # Kana (Katakana/Hiragana)
 source_deck_did = 1624247053854
 
-target_field = "Onyomi"
+target_field = "Image"
 
-target_note_type_name = "Japanese Kanji"
+target_note_type_name = "Some Note"
 
-value_replace = r""
+value_replace = r"""<img src="\1">"""
 
-replace_search = re.compile(r"""^なし""")
+replace_search = re.compile(r"""<\s*img\s*src\s*=\s*"(.*?)"\s*\/\s*>""")
 
-condition_field = r"EnglishAudio"
-condition_search = re.compile(r"""\]\[""")
+condition_field = r"Image"
+condition_search = re.compile(r"""<\s*img\s*src\s*=\s*"(.*?)"\s*\/\s*>""")
 
 for nid, in mw.col.db.execute(f"select id from notes"):
     note = mw.col.getNote(nid)
@@ -28,18 +28,19 @@ for nid, in mw.col.db.execute(f"select id from notes"):
         first_card = cards[0]
         deck_id = first_card.did
 
-        if deck_id == source_deck_did:
-            old_text = note[target_field]
-            condition_text = note[condition_field]
+        # if deck_id == source_deck_did:
+        old_text = note[target_field]
+        condition_text = note[condition_field]
 
-            condition_match = condition_search.search(condition_text)
-            if not condition_match:
-                # print(f"Skipping note {note.id} '{old_text}' -> '{condition_text}'...")
-                continue
+        condition_match = condition_search.search(condition_text)
+        if not condition_match:
+            # print(f"Skipping note {note.id} '{old_text}' -> '{condition_text}'...")
+            continue
 
-            replace_match = replace_search.search(old_text)
-            if replace_match:
-                new_text = replace_search.sub(value_replace, old_text)
-                print(f"Changing note {note.id} '{old_text}' -> '{replace_match.group(0)}' -> '{new_text}'...")
-                note[target_field] = new_text
-                note.flush()
+        replace_match = replace_search.search(old_text)
+        if replace_match:
+            new_text = replace_search.sub(value_replace, old_text)
+            print(f"Changing note {note.id} '{old_text}' -> '{replace_match.group(0)}' -> '{new_text}'...")
+            # break
+            note[target_field] = new_text
+            note.flush()
