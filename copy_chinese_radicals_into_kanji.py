c83bbadc895403e343434edb0006f02f941ca5ee
evandrocoan <evandrocoan@hotmail.com>
2022-12-11 21:19:22 -0300

    Add support to radical without kunyomi/onyomi

--- copy_chinese_radicals_into_kanji.py
+++ copy_chinese_radicals_into_kanji.py
@@ -6,7 +6,7 @@ g_radical_field = "Radical"
 kanji_notes = {}
 radical_notes = {}
 
-def getField(field, note):
+def stripField(field, note):
     fieldText = note[field]
     from anki.utils import stripHTML
     return stripHTML(fieldText) if fieldText else None
@@ -16,8 +16,8 @@ for nid, in mw.col.db.execute(f"select id from notes"):
     note_type = note.note_type()
 
     if note_type["name"] == "Japanese Kanji":
-        kanji_field_text = getField(g_kanji_field, note)
-        radical_field_text = getField(g_radical_field, note)
+        kanji_field_text = stripField(g_kanji_field, note)
+        radical_field_text = stripField(g_radical_field, note)
         assert kanji_field_text, f"Cannot have an empty kanji field '{g_kanji_field}={kanji_field_text}'"
 
         if kanji_field_text == radical_field_text:
@@ -42,20 +42,24 @@ for kanji_field_text, kanji_note in kanji_notes.items():
         else:
             source_field1, source_field2, target_field = fields
 
-        radical_field_text = getField(g_radical_field, kanji_note)
+        radical_field_text = stripField(g_radical_field, kanji_note)
 
         if radical_field_text:
             radical_note = radical_notes[radical_field_text]
 
-            if getField(source_field1, radical_note):
+            if stripField(source_field1, radical_note):
                 source_field = source_field1
             else:
                 assert source_field2
                 source_field = source_field2
 
+                if not stripField(source_field, radical_note):
+                    assert target_field
+                    source_field = target_field
+
             source_field_text = radical_note[source_field]
 
-            # target_field_text = getField(target_field, kanji_note)
+            # target_field_text = stripField(target_field, kanji_note)
             # if target_field_text:
             #     print(f"Skipping kanji '{kanji_field_text}[{target_field}]' because it is already filled!")
             # else:
