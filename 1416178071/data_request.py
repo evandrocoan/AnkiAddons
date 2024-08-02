f15aa4cb473c54633c4006b5ce5a74d204012851
evandrocoan <evandrocoan@hotmail.com>
2024-08-02 00:56:25 -0300

    Set to retry chat gpt request on rate limit

--- 1416178071/data_request.py
+++ 1416178071/data_request.py
@@ -9,6 +9,8 @@ vendor_dir = os.path.join(addon_dir, "vendor")
 sys.path.append(vendor_dir)
 import openai
 
+import time;
+from openai import error
 
 
 from html import unescape
@@ -38,12 +40,23 @@ def send_prompt_to_openai(prompt):
     try:
         print("Request to ChatGPT: ", prompt)
         openai.api_key = config['apiKey']
-        # gpt-3.5-turbo
-        # gpt-4o-mini
-        response = openai.ChatCompletion.create(model="gpt-4o", messages=[{"role": "user", "content": prompt}], max_tokens=2000)
-        print("Response from ChatGPT", response)
-        return response.choices[0].message.content.strip()
+
+        def try_call():
+            # gpt-3.5-turbo
+            # gpt-4o-mini
+            response = openai.ChatCompletion.create(model="gpt-4o", messages=[{"role": "user", "content": prompt}], max_tokens=2000)
+            print("Response from ChatGPT", response)
+            return response.choices[0].message.content.strip()
+
+        maximum = 300
+        while maximum > 0:
+            maximum -= 1
+            try:
+                return try_call()
+
+            except error.RateLimitError as e:
+                time.sleep(1.0)  # gpt-4o is has a token limit
 
     except Exception as e:
-        showWarning(f"An error occurred while processing the note: {str(e)}")
+        print(f"An error occurred while processing the note: {str(e)}", file=sys.stderr)
         return None
