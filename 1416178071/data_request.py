797f0433f59d698d6ec26756ee4616217ccec9d2
evandrocoan <evandrocoan@hotmail.com>
2024-08-18 16:52:06 -0300

    Added hard coded support to image generation on IntelliFiller ChatGPT addon

--- 1416178071/data_request.py
+++ 1416178071/data_request.py
@@ -60,3 +60,72 @@ def send_prompt_to_openai(prompt):
     except Exception as e:
         print(f"An error occurred while processing the note: {str(e)}", file=sys.stderr)
         return None
+
+
+def send_prompt_to_openai_image(prompt):
+    config = mw.addonManager.getConfig(__name__)
+    if config['emulate'] == 'yes':
+        print("Fake request chatgpt: ", prompt)
+        return f"This is a fake response for emulation mode for the prompt {prompt}."
+
+    try:
+        import requests
+        import json
+        import base64
+        import pathlib
+
+        print("Request to ChatGPT: ", prompt)
+        api_key = config['apiKey']
+        media_dir = pathlib.Path(mw.col.media.dir())
+
+        # https://platform.openai.com/docs/api-reference/images/create
+        url = "https://api.openai.com/v1/images/generations"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_key}"
+        }
+
+        data = {
+            "prompt": prompt,
+            "n": 1,
+            "model": 'dall-e-3',
+            "quality": 'hd',
+            "response_format": "b64_json",
+            "size": "1024x1024"  # Optional: can be adjusted to different sizes like "256x256", "512x512"
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
+        if response.status_code == 200:
+            response_json = response.json()
+            revised_prompt = response_json["data"][0]["revised_prompt"]
+            file_name = f"{revised_prompt[:100]}-{response_json['created']}"
+
+            # with open(media_dir / f"{file_name}.json", mode="w", encoding="utf-8") as file:
+            #     json.dump(response_json, file)
+
+            image_info = (f"""<img alt="generated image" src="{file_name}.png">"""
+                f"""\n:::plugin:::\n{response_json['created']},\n{revised_prompt},\n{file_name}.png"""
+            )
+            print(image_info)
+
+            image_data = base64.b64decode(response_json["data"][0]["b64_json"])
+            with open(media_dir / f"{file_name}.png", mode="wb") as png:
+                png.write(image_data)
+
+            # dalle 3 has request limit https://platform.openai.com/docs/guides/rate-limits/usage-tiers?context=tier-free
+            # check your tier on https://platform.openai.com/settings/organization/limits
+            time.sleep(10.0)
+            return image_info
+
+        print(f"Failed to get response: {response.status_code}")
+        print(response.text)
+        print(data)
+        return None
+
+    except Exception as e:
+        print(f"An error occurred while processing the note: {str(e)}", file=sys.stderr)
+        return None
+
+
+send_prompt_to_openai = send_prompt_to_openai_image
